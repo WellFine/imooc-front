983f166d9fe23c3ea8b9f400c5e4f69c95881b49
unknown <1339531047@qq.com>
2022-10-13 00:58:53 +0800

    feat: 构建项目架构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -13,7 +12,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import './styles/index.scss'
 import App from './App.vue'
 
