4dab6525e2a93312dd5f8ab64cbd3d79211c8c2b
unknown <1339531047@qq.com>
2022-10-14 01:00:02 +0800

    feat: 导入并配置 axios

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,3 +1,13 @@
 <template>
   <div>我是移动端下的 navigation</div>
 </template>
+
+<script setup>
+  import { getCategory } from '@/api/category'
+
+  const getCategoryData = async () => {
+    const res = await getCategory()
+    console.log(res)
+  }
+  getCategoryData()
+</script>
