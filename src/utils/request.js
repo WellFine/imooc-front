b505248067cd35c1be1cae2180ceb74713b50b59
unknown <1339531047@qq.com>
2022-10-15 22:21:57 +0800

    feat: 数据拦截简化数据获取流程

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,4 +16,17 @@ service.interceptors.request.use(config => {
   return config  // 设置后记得返回 config
 })
 
+/**
+ * 响应拦截器
+ * 服务端返回数据之后，数据在 Promise 结构被 .then 或 await 前执行
+ */
+service.interceptors.response.use(response => {
+  const { success, message, data } = response.data
+  if (success) {  // success 为 true 表明业务请求成功
+    return data
+  }
+  // TODO：业务请求错误
+  return Promise.reject(new Error(message))
+})
+
 export default service
