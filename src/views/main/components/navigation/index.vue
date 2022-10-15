5ee4f71468371fcbaa8e17258f503b7a9af7318a
unknown <1339531047@qq.com>
2022-10-16 00:50:09 +0800

    feat: 业务组件：移动端 navigationBar

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <mobile-navigation-vue v-if="isMobileTerminal" />
+  <mobile-navigation-vue v-if="isMobileTerminal" :data="categoryData" />
   <pc-navigation-vue v-else />
 </template>
 
 <script setup>
+  import { ref } from 'vue'
   import { isMobileTerminal } from '@/utils/flexible'
   import mobileNavigationVue from './mobile/index.vue'
   import pcNavigationVue from './pc/index.vue'
+  import { getCategory } from '@/api/category'
+
+  const categoryData = ref([])
+
+  const getCategoryData = async () => {
+    const { categorys } = await getCategory()
+    categoryData.value = categorys
+  }
+  getCategoryData()
 </script>
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>我是移动端下的 navigation</div>
+  <div class="bg-white sticky top-0 left-0 z-10">
+    <ul class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
+      <li
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200"
+        v-for="item in data" :key="item.id"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-  import { getCategory } from '@/api/category'
-
-  const getCategoryData = async () => {
-    const res = await getCategory()
-    console.log(res)
-  }
-  getCategoryData()
+  /**
+   * defineProps 方法接收与 props 选项相同的值，以此获得 props 的完整类型推导支持
+   * 该方法只能在 <script setup> 单文件组件中使用，可以直接使用无需导入
+   */
+  defineProps({
+    data: {
+      type: Array,
+      required: true
+    }
+  })
 </script>
