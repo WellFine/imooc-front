5beb567e15e081c1178f8687ea24d5d74c89eb04
unknown <1339531047@qq.com>
2022-10-16 22:42:33 +0800

    feat: vite 通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,13 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install (app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popup)
+    // 获取所有通用组件的 index.vue，import.meta.glob 是 vite 提供的
+    const components = import.meta.glob('./*/index.vue')
+    for (const [fullPath, fn] of Object.entries(components)) {
+      // fullPath 都是 './组件名/index.vue' 形式，所以在新建通用组件时，文件夹名称就是组件名
+      const componentName = `m-${fullPath.split('/')[1]}`
+      app.component(componentName, defineAsyncComponent(fn))
+    }
   }
 }
