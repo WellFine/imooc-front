b722f1f5b8b60ef10837523b23693895c4b0289a
unknown <1339531047@qq.com>
2022-10-19 14:41:56 +0800

    feat: 基于 popover 处理用户模块

--- src/views/layout/components/header/header-my.vue
+++ src/views/layout/components/header/header-my.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>my</div>
+  <m-popover class="flex items-center" placement="bottom_left">
+    <template #reference>
+      <div class="relative flex items-center p-0.5 rounded-sm cursor-pointer duration-200 outline-none hover:bg-zinc-100">
+        <!-- 头像 -->
+        <img class="w-3 h-3 rounded-sm" src="https://blog.xqtcat.cn/images/logo.jpg" />
+        <!-- 箭头 -->
+        <m-svg-icon class="h-1 w-1 ml-0.5" name="down-arrow" fillClass="fill-zinc-900" />
+        <!-- vip 图标 -->
+        <m-svg-icon class="h-1.5 w-1.5 absolute right-[16px] bottom-0" name="vip" />
+      </div>
+    </template>
+    <!-- 气泡 -->
+    <div class="w-[140px] overflow-hidden">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60"
+        v-for="item in menuArr" :key="item.id"
+      >
+        <m-svg-icon class="w-1.5 h-1.5 mr-1" :name="item.icon" fillClass="fill-zinc-900" />
+        <span class="text-zinc-800 text-sm">{{ item.title }}</span>
+      </div>
+    </div>
+  </m-popover>
 </template>
+
+<script setup>
+  const menuArr = [
+    { id: '0', title: '个人资料', icon: 'profile', path: '/profile' },
+    { id: '1', title: '升级 VIP', icon: 'vip-profile', path: '/member' },
+    { id: '2', title: '退出登录', icon: 'logout', path: '' }
+  ]
+</script>
