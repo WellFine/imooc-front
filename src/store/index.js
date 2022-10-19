f151f5f448c8ed30c247f2639169797c3dc52ccd
unknown <1339531047@qq.com>
2022-10-20 01:49:56 +0800

    feat: category 数据缓存，覆盖初始数据

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,19 @@
 import { createStore } from 'vuex'
 import getters from './getters'
 import category from './modules/category'
+import createPersistedState from 'vuex-persistedstate'
 
 const store = createStore({
   getters,
   modules: {
     category
-  }
+  },
+  plugins: [
+    createPersistedState({  // 使用这个 plugin 后会自动到 localStorage 中加载对应模块的缓存数据
+      key: 'imooc-front',  // 指定保存到 localStorage 中的 key
+      paths: ['category']  // 需要保存的模块
+    })
+  ]
 })
 
 export default store
