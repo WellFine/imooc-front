9ff2808c794292497f21b67984e1bdbab1ec9727
unknown <1339531047@qq.com>
2022-10-20 01:26:29 +0800

    feat: PC 端 navigationBar 私有逻辑处理

--- src/views/main/components/navigation/pc/index.vue
+++ src/views/main/components/navigation/pc/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="bg-white sticky top-0 left-0 w-full z-10">
-    <ul class="w-[800px] relative flex flex-wrap justify-center overflow-x-auto px-[10px] py-1 text-xs text-zinc-600 duration-300 overflow-hidden mx-auto">
+    <ul
+      class="w-[800px] relative flex flex-wrap justify-center overflow-x-auto px-[10px] py-1 text-xs text-zinc-600 duration-300 overflow-hidden mx-auto"
+      :class="[isOpenCategory ? 'h-[206px]' : 'h-[56px]']"
+    >
       <!-- 箭头图标 -->
-      <div class="absolute right-1 bottom-2.5 z-20 p-1 rounded cursor-pointer duration-200 hover:bg-zinc-200">
-        <m-svg-icon name="unfold" class="w-1 h-1" fillClass="fill-zinc-900" />
+      <div class="absolute right-0 bottom-1 z-20 p-1 rounded cursor-pointer duration-200 hover:bg-zinc-200" @click="toggle">
+        <m-svg-icon :name="isOpenCategory ? 'fold' : 'unfold'" class="w-1 h-1" fillClass="fill-zinc-900" />
       </div>
       <!-- leading 是 line-height -->
       <li
-        v-for="item in $store.getters.categorys" :key="item.id"
+        v-for="(item, index) in $store.getters.categorys" :key="item.id"
         class="shrink-0 px-1.5 py-0 z-10 duration-200 text-zinc-900 text-base font-bold h-4 leading-4 cursor-pointer rounded mr-0.5 mb-1 hover:bg-zinc-200"
+        :class="{ 'text-zinc-900 bg-zinc-200': index === currentCategoryIndex }"
+        @click="onItemClick(index)"
       >{{ item.name }}</li>
     </ul>
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  // 是否打开所有分类数据
+  const isOpenCategory = ref(false)
+  const toggle = () => {
+    isOpenCategory.value = !isOpenCategory.value
+  }
+
+  // 选中分类下标
+  const currentCategoryIndex = ref(0)
+  const onItemClick = index => {
+    currentCategoryIndex.value = index
+  }
+</script>
