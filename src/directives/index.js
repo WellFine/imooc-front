e60c40d4e21b168783c9e12b26c6d1c09606b7e9
unknown <1339531047@qq.com>
2022-11-02 17:35:13 +0800

    feat: 深入 vite：指令的自动注册

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,8 +1,10 @@
-import lazy from './modules/lazy'
-
 // 全局注册指令
 export default {
   install (app) {
-    app.directive('lazy', lazy)
+    const directives = import.meta.globEager('./modules/*.js')
+    for (const [key, value] of Object.entries(directives)) {
+      // key: './modules/lazy.js' => directiveName: lazy
+      app.directive(key.split('/').pop().replace('.js', ''), value.default)
+    }
   }
 }
