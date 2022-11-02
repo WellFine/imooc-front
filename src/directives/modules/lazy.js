ddb4785c5d968ed0332d13dcbe079b2a5bd251e2
unknown <1339531047@qq.com>
2022-11-02 17:54:11 +0800

    feat: 指定彩色占位图

--- src/directives/modules/lazy.js
+++ src/directives/modules/lazy.js
@@ -1,13 +1,16 @@
 import { useIntersectionObserver } from '@vueuse/core'
+import { randomRGB } from '@/utils/color'
 
 // 图片懒加载指令：图片不可见时不加载，图片可见时才加载
 export default {
   mounted (el) {
     const imgSrc = el.src
-    el.src = ''  // 将 src 置为空，图片就不会加载，这里也可以指定为占位图片
+    el.src = ''  // 将 src 置为空，图片就不会加载
+    el.style.backgroundColor = randomRGB()  // 指定占位随机颜色
     const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
       if (isIntersecting) {
         el.src = imgSrc  // 图片可见时加载图片
+        el.style.backgroundColor = ''
         stop()  // 停止监听 img 是否可见
       }
     })
