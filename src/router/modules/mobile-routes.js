bb1d5c42fdd818f203edff109449729420a858fc
unknown <1339531047@qq.com>
2022-11-10 18:59:10 +0800

    feat: 处理刷新空白问题

--- src/router/modules/mobile-routes.js
+++ src/router/modules/mobile-routes.js
@@ -5,4 +5,8 @@ export default [{
   path: '/',
   name: 'home',
   component: () => import('@/views/main/index.vue')
+}, {
+  path: '/pins/:id',
+  name: 'pins',
+  component: () => import('@/views/pins/index.vue')
 }]
--- src/router/modules/pc-routes.js
+++ src/router/modules/pc-routes.js
@@ -10,4 +10,8 @@ export default [{
     name: 'home',
     component: () => import('@/views/main/index.vue')
   }]
+}, {
+  path: '/pins/:id',
+  name: 'pins',
+  component: () => import('@/views/pins/index.vue')
 }]
