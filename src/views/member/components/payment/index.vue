8fbde4f9a9355ec70e0272ed03688d588c6cd231
unknown <1339531047@qq.com>
2023-03-24 10:50:48 +0800

    feat: 支付模块：移动端支付样式处理

--- src/views/member/components/payment/index.vue
+++ src/views/member/components/payment/index.vue
@@ -2,10 +2,13 @@
   <div>
     <!-- PC 端支付 -->
     <pc-payment-vue v-if="!isMobileTerminal" />
+    <!-- 移动端支付 -->
+    <mobile-payment-vue v-else />
   </div>
 </template>
 
 <script setup>
   import { isMobileTerminal } from '@/utils/flexible'
   import pcPaymentVue from './pc-payment/index.vue'
+  import mobilePaymentVue from './mobile-payment/index.vue'
 </script>
---
+++ src/views/member/components/payment/mobile-payment/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="fixed left-0 bottom-0 w-screen text-center bg-white dark:bg-zinc-800 xl:hidden">
+    <!-- 特惠提示 -->
+    <discounts-vue />
+    <!-- 支付 -->
+    <div class="flex justify-between text-xs px-1 py-0.5">
+      <div class="text-left text-zinc-900 dark:text-zinc-200">
+        <p>
+          <span>券后合计：</span>
+          <span class="text-red-600 text-[16px] font-sans font-medium">￥</span>
+          <span class="text-red-600 text-[22px] font-sans font-medium">9</span>
+        </p>
+        <p class="text-red-600">优惠券：限时立减￥10</p>
+      </div>
+      <m-button class="w-[120px]" :isActiveAnimation="false" @click="onConfirmClick">立即开通</m-button>
+    </div>
+    <m-popup v-model="isOpenPopup" class="rounded">
+      <mobile-pay-select-vue />
+    </m-popup>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import discountsVue from '../discounts.vue'
+  import mobilePaySelectVue from './mobile-pay-select.vue'
+
+  const isOpenPopup = ref(false)
+  const onConfirmClick = () => {
+    isOpenPopup.value = true
+  }
+</script>
